17562ff6a4927600a7febf4a5e226ad0b3bad663
Karthik Rajasekaran <nuskarthik@gmail.com>
2014-01-26 05:47:00 +0800

    added profile.py logic

--- app/feed.py
+++ app/feed.py
@@ -1,7 +1,13 @@
 import flask, flask.views
+import database
+from sqlalchemy import desc
 
 class Feed(flask.views.MethodView):
 	def get(self):
+		challengesPosted = database.Challenges.query(),order_by(desc(Challenges.createdTime)).all()
+		name= challengesPosted.challengeName
+		level= challengesPosted.challengeLevel 
+		chalDict = [{}]
 		return flask.render_template('feed.html')
 
 	def acceptChallenge(self, id):
--- app/profile.py
+++ app/profile.py
@@ -1,8 +1,13 @@
 import flask, flask.views
 # from database import *
+from app import database
 
 class Profile(flask.views.MethodView):
 	def get(self):
+		#name = 
+		# Level = 
+		#Experience
+		#money
 		# created = 
 		# accepted = get from the db
 		# completed = get from the db
@@ -10,7 +15,53 @@ class Profile(flask.views.MethodView):
 		# while (there are tuples):
 		# 	pending.title		
 		# 	pending.timeLeft
-		return flask.render_template('profile.html')
+		#	pending.id
+		result = database.UserChallengeJoin.query.all()
+		to_do = []
+		counter = 0
+		accepted = 0
+		for check in result:
+			if check.email ==session['email']:
+				if check.status==0:
+					counter+=1
+				if check.status==1:
+					accepted+=1
+					todo.append(check.challengeId)
+		userStat = database.Users.query.get(session['index'])
+		name = userStat.userName
+		level = userStat.userLevel
+		xp = userStat.experience
+		money = userStat.money
+		xprequired = userStat.userLevel * 1000
+
+		pending_title = []
+		pending_timeLeft = []
+		won = 0
+		for chalId in to_do:
+			checkChallenges = database.Challenges.query.get(chalId)
+			pending_title.append(checkChallenges.challengeName)	
+			pending_timeLeft.append(checkChallenges.endTime.strftime("%A, %d. %B %Y %I:%M%p"))
+ 
+
+		#won- challenge id = sub query  and answer.status = 1
+		checkAnswers = database.Answers.query.all()
+		for ans in checkAnswers:
+			if ans.challengeId in to_do and ans.status = True:
+				won+=1
+
+		profileDict.update( {'name': name,  
+							'level':level, 
+							'xp':xp,
+							'xprequired':xprequired,
+							'money':money,
+							'to_do': to_do,
+							'pending_title': pending_title,
+							'pending_timeLeft': pending_timeLeft,
+							'created': created,
+							'accepted': accepted,
+							'completed':won	 )
+
+		return flask.render_template('profile.html', profileDict)
 
 
 	def post(self):
