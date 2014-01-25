cf03a7629d238f207359e22ade5d436762069c14
Raghav Ramesh <raghavrnus@gmail.com>
2014-01-26 01:38:13 +0800

    fix(connection) hk's server

--- app/profile.py
+++ app/profile.py
@@ -1,8 +1,15 @@
 import flask, flask.views
+# from database import *
 
 class Profile(flask.views.MethodView):
 	def get(self):
-
+		# created = 
+		# accepted = get from the db
+		# completed = get from the db
+		# xp = get from the db
+		# while (there are tuples):
+		# 	pending.title		
+		# 	pending.timeLeft
 		return flask.render_template('profile.html')
 
-	
+		
