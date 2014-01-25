591e4cb4883c708065b4d538eb84f2ba8fc4423a
Raghav Ramesh <raghavrnus@gmail.com>
2014-01-26 02:00:32 +0800

    feat(logic) template of logic functions

--- app/challengeInfo.py
+++ app/challengeInfo.py
@@ -2,4 +2,20 @@ import flask, flask.views
 
 class ChallengeInfo(flask.views.MethodView):
 	def get(self):
+
 		return flask.render_template('challengeInfo.html')
+
+	def acceptChallenge(self):
+		print "acceptChallenge"
+
+	def upvoteChallenge(self):
+		print "upvoteChallenge"
+
+	def downvoteChallenge(self):
+		print "downvoteChallenge"
+
+	def commentChallenge(self):
+		print "commentChallenge"
+
+	def uploadAnswer(self):
+		print "uploadAnswer"
