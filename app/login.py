309d81830341f0a59003ba3272a402de3dd4bd21
Vignesh Rajakumar <vigneshr.93@gmail.com>
2014-01-26 03:58:46 +0800

    login

--- app/login.py
+++ app/login.py
@@ -1,14 +1,18 @@
 import flask, flask.views
-import requests
-import database
+from flask import request
+from database import Users
 
 class Login(flask.views.MethodView):
+	def __init__(self):
+		self.users = Users()
+
 	def get(self):
 		return flask.render_template('login.html')
 
 	def post(self):
 		
-		success = database.User.loginCheck(request.form['username'],request.form['password'])
+		print request.form['username']
+		success = self.users.loginCheck(request.form['username'],request.form['password'])
 		print success
 		if(success):
 			return flask.render_template('profile.html')
