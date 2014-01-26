00b3b63fc74f3ac6b4719ece6a141bc7ecf92f9f
Vignesh Rajakumar <vigneshr.93@gmail.com>
2014-01-26 13:07:40 +0800

    Fix bug. correct database adding challenge

--- app/challengeCreate.py
+++ app/challengeCreate.py
@@ -1,7 +1,8 @@
 import flask, flask.views
 from flask import request, url_for
 import datetime
-import database
+from database import Challenges,db,UserChallengeJoin
+from flask import session
 
 class ChallengeCreate(flask.views.MethodView):
 	def get(self):
@@ -9,16 +10,22 @@ class ChallengeCreate(flask.views.MethodView):
 
 	def post(self):
 		print "createChallenge"
-
+		print session['email']
 		title= request.form['title']
+		print 'titl'
 		details = request.form['details']
+		print 'det'
 		level = request.form['level']
+		print 'level'
 		startTime = datetime.date.today()
 		endTime = datetime.date.today() + datetime.timedelta(days=5)
 		print "comes here"
-		chl = database.Challenges(challengeName = title, description = details, challengeLevel = "level", createdTime = startTime, startTime = startTime, endTime = endTime)
+		chl = Challenges(challengeName = title,createdEmail = session['email'], description = details, challengeLevel = "level", createdTime = startTime, startTime = startTime, endTime = endTime)
 		print chl
 		db.session.add(chl)
+		db.session.commit()
+		chj = UserChallengeJoin(email = session['email'], challengeId = chl.challengeId, status = 'created')
+		db.session.add(chj)
 		db.session.commit()
 		message = "Challenge created successfully!"
 		return flask.render_template('challengeCreate.html')
