8b750229d2cb6169ed4846906c39f01f1e6e703c
Raghav Ramesh <raghavrnus@gmail.com>
2014-01-26 09:58:40 +0800

    fix(challengeInfo) db values

--- app/challengeInfo.py
+++ app/challengeInfo.py
@@ -1,5 +1,7 @@
 import flask, flask.views
 import datetime	
+import database
+from flask import request
 
 class ChallengeInfo(flask.views.MethodView):
 	def get(self):
@@ -7,13 +9,14 @@ class ChallengeInfo(flask.views.MethodView):
 		cmts = self.getComments()
 		ans = self.getAnswers()
 		challengeDict = dict()
-		challengeDict.update( {'basic': basic, 'cmts':cmts, 'ans':ans}
-		return flask.render_template('challengeInfo.html', challengeDict= challengeDict)
+		challengeDict.update( {'basic': basic, 'cmts':cmts, 'ans':ans } )
+		print challengeDict
+		return flask.render_template('challengeInfo.html', challengeDict=challengeDict)
 
 	def acceptChallenge(self):
 		print "acceptChallenge"
 		email = request.args.get('username')
-		chl = UserChallengeJoin(challengeId = self.challengeId,email = email, status =  )
+		chl = UserChallengeJoin(challengeId = self.challengeId, email = email, status = 'accepted')
 		db.session.add(chl)
 		db.session.commit()
 		message = "You have accepted this challenge"
@@ -31,15 +34,16 @@ class ChallengeInfo(flask.views.MethodView):
 		print "uploadAnswer"
 
 	def getBasic(self):
-		resultSet = Challenges.query.filter(Challenges.challengeId== self.challengeId)
+		resultSet = database.Challenges.query.filter(database.Challenges.challengeId == request.args.get('id')).all()
 		return resultSet
 
 	def getComments(self):
-		resultSet = ChallengeComment.query.filter(ChallengeComment.challengeId = self.challengeId)
+		resultSet = database.ChallengeComment.query.filter(database.ChallengeComment.challengeId == request.args.get('id')).all()
 		return resultSet
 
 	def getAnswers(self):
-		resultSet = Answers.query.filter(Answers.challengeId = self.challengeId)
+		resultSet = database.Answers.query.filter(database.Answers.challengeId == request.args.get('id')).all()
 		return resultSet
+
 	def getOwner(self):
-		user = Challenges.query.get(Challenges.challengeId== self.challengeId )
+		user = database.Challenges.query.get(database.Challenges.challengeId == request.args.get('id'))
